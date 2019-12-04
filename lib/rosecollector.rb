e33d2b3150e3468ff19033f2be601d78a1152a3e
Karen Vo <karenvo@usc.edu>
2019-12-03 17:10:48 -0800

    first commit

---
+++ lib/rosecollector.rb
@@ -0,0 +1,6 @@
+require "rosecollector/version"
+
+module Rosecollector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rosecollector/version.rb
@@ -0,0 +1,3 @@
+module Rosecollector
+  VERSION = "0.1.0"
+end
